e736d164fb09e710a18d12a8f96b2574aba4525b
repleyva <rusvelpassos@gmail.com>
2022-06-04 15:45:34 -0500

    add coin list screen

---
+++ app/src/main/java/repleyva/dev/cryptocurrency/presentation/ui/coin_list/CoinListScreen.kt
@@ -0,0 +1,55 @@
+package repleyva.dev.cryptocurrency.presentation.ui.coin_list
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import repleyva.dev.cryptocurrency.domain.model.Coin
+import repleyva.dev.cryptocurrency.presentation.ui.coin_list.components.CoinListItem
+
+@Composable
+fun CoinListScreen(
+    viewModel: CoinListViewModel = hiltViewModel(),
+    toDetail: (Coin) -> Unit,
+) {
+    val state = viewModel.state.value
+    Box(modifier = Modifier.fillMaxSize()) {
+        LazyColumn(modifier = Modifier.fillMaxSize()) {
+            items(state.coins) { coin ->
+                CoinListItem(
+                    coin = coin,
+                    onItemClick = { toDetail(coin) }
+                )
+            }
+        }
+        if (state.error.isNotBlank()) {
+            Text(
+                text = state.error,
+                color = MaterialTheme.colors.error,
+                textAlign = TextAlign.Center,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 20.dp)
+                    .align(Alignment.Center)
+            )
+        }
+        if (state.isLoading) {
+            CircularProgressIndicator(
+                modifier = Modifier.align(Alignment.Center),
+                color = Color.Green
+            )
+        }
+    }
+}
